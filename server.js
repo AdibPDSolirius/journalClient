29301d8a592f4dab324eb19905e43e5ef0184c1d
Adib <adib.pournazari@solirius.com>
2018-10-05 16:26:51 +0100

    moved post install script about node start

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const app = express();
 app.use(express.static(__dirname + '/dist/journalClient'));
 
 app.get('/*', function(req,res) {
-  console.log('reached');
   res.sendFile(path.join(__dirname+'/dist/journalClient/index.html'));
 });
 
