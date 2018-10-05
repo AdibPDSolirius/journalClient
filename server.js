6d34d7f507c9fa5996ed9cc0e2c80913736567a5
Adib <adib.pournazari@solirius.com>
2018-10-05 16:08:38 +0100

    Fixed link to index.html

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/journalApplication'));
+app.use(express.static(__dirname + '/dist/journalClient'));
 
 app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/dist/journalApplication/index.html'));
+  console.log('reached');
+  res.sendFile(path.join(__dirname+'/dist/journalClient/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
